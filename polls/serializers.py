1a3e61e11ffa5b1622a286d79fce82a2b851caf5
“Pat <pat.mansfield@hotmail.com>
2018-01-21 15:54:42 +0000

    Import changes

--- polls/serializers.py
+++ polls/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework import serializers
-from .models import Vote, PollSubject, Poll
+from polls.models import Vote, PollSubject, Poll
 
 
 class VoteSerializer(serializers.ModelSerializer):
